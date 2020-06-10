35ffc6560dcb10830de8769b2ffb3e7432621920
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-06-10 23:35:38 +0200

    Adding colors and new confirmation + push

--- git-push-all-remotes.sh
+++ git-push-all-remotes.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 AR=($(git remote))
+greenColor=`tput setaf 2`
 function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
 confirm() {
-    # call with a prompt string or use a default
     read -r -p "${1:-Are you sure? [y/N]} " response
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
+    response=${response,,} # tolower
+    if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+       true
+    fi
 }
 
-
-
-# echo "$my_array_length"
-
-
-confirm "Would you really like to do a push? On these remotes $(join_by ', ' ${AR[@]}) ? (y/n)" 
+echo "$(tput setaf 6)$(git rev-parse --abbrev-ref HEAD) -> $(join_by ', ' ${AR[@]})$(tput sgr0)"
+confirm "Would you really like to do a push? On these remotes $(join_by ', ' ${AR[@]}) ? (y/n)"
 if [[ $? -eq 0 ]]; then
-  echo 'Pushing to your remotes'  
-  for i in "${!AR[@]}"; do   
-    eval git remote get-url ${AR[$i]}  
-  done
+    echo "${greenColor}Pushing to your remotes"
+    for i in "${!AR[@]}"; do
+        eval tput setaf 0
+        eval git push ${AR[$i]} $(git rev-parse --abbrev-ref HEAD)
+    done
 fi
-
-
-# for remoteLine in "${!AR[@]}"
-# do
-# printf "%s\t%s\n" "$i" "${foo[$i]}"
-# echo "$remoteLine"
-
-
-
-# done
